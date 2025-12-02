49f2db44923d510a41d3471c4714c6c4ecd52c0c
Prerana <164289574+prerana059@users.noreply.github.com>
2025-12-02 19:23:33 +0545

    Update page.tsx for navigation error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import LoginForm from "../../components/LoginForm";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen bg-white flex flex-col items-center">
       <div className="flex items-center justify-center pt-8 pb-4 gap-4">
-        <img className="w-[400px]" src="/images/letsgetstarted.png" alt="Logo" />
+        <img
+          className="w-[400px]"
+          src="/images/letsgetstarted.png"
+          alt="Logo"
+        />
       </div>
       <div className="auth-inner">
         <LoginForm />
@@ -12,5 +18,7 @@ export default function LoginPage() {
     </div>
   );
 }
+
+
 
 
